191c6c9e7a16836ac036a73b788235e179ab0e52
Ondrej <ondrejsach@gmail.com>
2021-08-09 22:01:03 +0200

    App.js - "Added styles for rendered items"

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, SafeAreaView, FlatList, ActivityIndicator, Image} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView, FlatList, ActivityIndicator, Image, Button} from 'react-native';
+import _ from "lodash"
 
 const dataURL = "https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/movies.json";
 const imgURL = "https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/public/images/";
@@ -9,6 +9,7 @@ const App = () => {
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [ direction, setDirection ] = useState(null)
 
   useEffect(() => {
     fetch(dataURL)
@@ -20,24 +21,35 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      {isLoading ? <ActivityIndicator /> : <FlatList
+      {isLoading ? <ActivityIndicator size="large" animating /> : <FlatList
           data={data}
           keyExtractor={(item, index) => item.episode_number}
+          extraData={data}
           renderItem={({item}) => {
             return(
-                <View style={styles.container}>
-                  <Text>
+                <View style={styles.item}>
+                  <Text style={styles.title}>
                     Title: {item.title}
                   </Text>
+                  {/* Epizoda je zminena v titulku
                   <Text>
                     Number of episode: {item.episode_number}
-                  </Text>
+                  </Text>*/}
                   <Image source={{uri: imgURL+item.poster}} style={styles.logo}/>
                 </View>
             )}}
       />}
+      <Button title='Sort by episodes' onPress={sortListByEpisodes} color='black' ></Button>
     </SafeAreaView>
   );
+
+  function sortListByEpisodes () {
+    const newDirection = direction === "desc" ? "asc" : "desc"
+    let sortedData = _.orderBy(data, ['episode_number'],[newDirection])
+    setDirection(newDirection)
+    setData(sortedData)
+    console.log(sortedData) /*Vypis serazenych prvku podle epizod*/
+  }
 }
 
 const styles = StyleSheet.create({
@@ -47,9 +59,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    backgroundColor: '#E5E4E2',
+    alignItems: 'center',
+    padding: 15,
+    margin: 10,
+    borderRadius: 10,
+  },
   logo: {
     width: 75,
-    height: 75
+    height: 120,
+    borderRadius: 10
+  },
+  title:{
+    fontSize: 15,
+    fontWeight: 'bold',
+    margin: 5
   }
 });
 
